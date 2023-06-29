69a5d7311a98041b3846158287193d8ee2f786ca
Kiara Eunice Abamonga <kiara.abamonga@gmail.com>
2023-06-29 13:13:43 -0400

    Initial Commit

---
+++ FinalProjectA2/FinalProjectA2App.swift
@@ -0,0 +1,17 @@
+//
+//  FinalProjectA2App.swift
+//  FinalProjectA2
+//
+//  Created by Kiara on 6/29/23.
+//
+
+import SwiftUI
+
+@main
+struct FinalProjectA2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
